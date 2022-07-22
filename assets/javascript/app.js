d48567052099b4f80b2a9a987ae625957efba796
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-07-22 12:09:33 +0100

    Added the back to top button to all footers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,3 +32,11 @@ function callArchiSearch() {
     // "https://www.archiuk.com/placenames"
   );
 }
+
+// Scroll to top of page button in footer
+toTheTopButton = document.getElementById("to_the_top_button");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
